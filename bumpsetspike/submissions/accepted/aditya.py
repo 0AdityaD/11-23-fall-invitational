95ab1c149fd2b682dbd0f99e0beedbf33b04e701
Aditya Durvasula <d4aditya@gmail.com>
2019-11-18 18:32:49 -0600

    fixed

--- bumpsetspike/submissions/accepted/aditya.py
+++ bumpsetspike/submissions/accepted/aditya.py
@@ -6,11 +6,11 @@ def solve():
     violations = []
     for i in range(n):
         if i + 4 <= n:
-            if A[i][0] == A[i + 1][0] == A[i + 2][0] == A[i + 3][0]:
+            if A[i][0] == A[i + 1][0] == A[i + 2][0] == A[i + 3][0] and (i == 0 or A[i - 1][0] != A[i][0]):
                 violations.append((i + 3, A[i][0]))
     for i in range(n):
         if i + 2 <= n:
-            if A[i][:2] == A[i + 1][:2]:
+            if A[i][:2] == A[i + 1][:2] and (i == 0 or A[i - 1][:2] != A[i][:2]):
                 violations.append((i + 1, A[i][0]))
     for i in range(n): 
         if i + 3 <= n and A[i][0] == A[i + 1][0] == A[i + 2][0] and (i == 0 or A[i - 1][0] != A[i][0]):
