95455a3d6e784ace407a4d78c132590bac0f773a
Ethan Arnold <masonsbro456@gmail.com>
2019-11-20 00:58:00 -0500

    Add Undetermined Determinant

---
+++ undetermineddeterminant/data/gen.py
@@ -0,0 +1,66 @@
+import random
+import os
+import shutil
+import subprocess
+import time
+import zipfile
+
+
+random.seed(12383)
+tc = 0
+
+def writeTestCase(data, sample=False):
+    global tc
+    tc += 1
+    print(tc)
+    if sample:
+        filename = 'sample/%02d.in' % tc
+        outputfilename = 'sample/%02d.ans' % tc
+    else:
+        filename = 'secret/%02d.in' % tc
+        outputfilename = 'secret/%02d.ans' % tc
+
+    with open(filename, 'w') as fout:
+        print(len(data), file=fout)
+        print('\n'.join(data), file=fout)
+    with open(outputfilename, 'w') as outf:
+        with open(filename, 'r') as inf:
+            subprocess.call(['python3', '../submissions/accepted/ethan.py'],
+                            stdin=inf, stdout=outf)
+
+
+def prep():
+    samplepath = os.getcwd() + '/sample'
+    secretpath = os.getcwd() + '/secret'
+    shutil.rmtree(samplepath, True)
+    shutil.rmtree(secretpath, True)
+    os.mkdir(samplepath)
+    os.mkdir(secretpath)
+    time.sleep(1)
+
+
+def sample1():
+    data = ['1']
+    return data
+
+
+def sample2():
+    data = ['2']
+    return data
+
+def main():
+    global tc
+    prep()
+    writeTestCase(sample1(), True)
+    writeTestCase(sample2(), True)
+
+    tc = 0
+    # generate long sequences with no violations
+    writeTestCase(['1000000'], False)
+    writeTestCase(['999999'], False)
+    writeTestCase(['999998'], False)
+    for _ in range(27):
+        writeTestCase([str(random.randint(1, 1000000))], False)
+
+if __name__ == '__main__':
+    main()
