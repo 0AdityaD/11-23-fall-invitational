d9da14fb07a2d69661485f76fd6ba75626b9faf0
Kevin Chen <kevinbc@gmail.com>
2019-11-20 21:12:13 -0600

    fix undetermineddeterminants solution

--- undetermineddeterminant/submissions/accepted/ethan.py
+++ undetermineddeterminant/submissions/accepted/ethan.py
@@ -9,7 +9,7 @@ for i in range(1,1000001):
 if n % 2 == 1:
     print(0)
 elif n == 2:
-    print(1)
+    print(-1 + MOD)
 else:
     n2 = n//2
     ans = pow(n2, n2-2,MOD)*pow(2,n-2,MOD)*fact[n]*pow(fact[n2],MOD-2,MOD)*pow(pow(2,n2,MOD),MOD-2,MOD)*pow(pow(n,n-2,MOD),MOD-2,MOD)%MOD
